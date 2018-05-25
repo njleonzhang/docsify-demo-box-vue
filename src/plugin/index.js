aaeffb17c8a254a78474822efa0d539c61f7f88b
njleonzhang <scream3616@sina.com>
2018-05-25 14:54:06 +0800

    prevent conflict to other code block related plugin

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,23 +1,34 @@
 import Vue from 'vue'
 import {generateComponent} from '../components/'
 
+let createCodeFn = function(oCodeFn) {
+  let id = 0
+
+  return function(code, lang) {
+    if (/^\s*\/\*\s*vue\s*\*\//.test(code)) {
+      id++
+      var DemoBlockWrapper = generateComponent(code, lang, jsResources, cssResources, bootCode)
+      Vue.component('DemoBox' + id, DemoBlockWrapper)
+      return '<' + 'demo-box-' + id + '></demo-box-' + id + '>'
+    } else {
+      if (oCodeFn) {
+        return oCodeFn.apply(this, arguments)
+      } else {
+        lang = lang || ''
+        var hl = Prism.highlight(code, Prism.languages[lang] || Prism.languages.markup)
+        return '<pre v-pre data-lang="' + lang + '"><code class="lang-' + lang + '">' + hl + '</code></pre>'
+      }
+    }
+  }
+}
+
 export let create = function(jsResources, cssResources, bootCode) {
   return function(hook, vm) {
-    let id = 0
     window.$docsify.markdown = {
       renderer: {
-        code: function(code, lang) {
-          if (/^\s*\/\*\s*vue\s*\*\//.test(code)) {
-            id++
-            var DemoBlockWrapper = generateComponent(code, lang, jsResources, cssResources, bootCode)
-            Vue.component('DemoBox' + id, DemoBlockWrapper)
-            return '<' + 'demo-box-' + id + '></demo-box-' + id + '>'
-          } else {
-            lang = lang || ''
-            var hl = Prism.highlight(code, Prism.languages[lang] || Prism.languages.markup)
-            return '<pre v-pre data-lang="' + lang + '"><code class="lang-' + lang + '">' + hl + '</code></pre>'
-          }
-        }
+        code: createCodeFn(window.$docsify.markdown &&
+          window.$docsify.markdown.renderer &&
+        window.$docsify.markdown.renderer.code)
       }
     }
   }
